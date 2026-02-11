b913f91ecda15d812090b9ec94e5f8c139a322f3
elsieen <elsiechang01@gmail.com>
2026-02-11 15:43:21 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    // ✅ GitHub Pages 必填：repo 名稱要完全一致
     base: '/sinomicrobranch_app_v2/',
-
+    plugins: [react()],
     server: {
       port: 3000,
       host: '0.0.0.0',
     },
-    plugins: [react()],
     define: {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
